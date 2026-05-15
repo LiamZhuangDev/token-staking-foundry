cb1fb33af60dd4eb0da94eb83b142251b57c9883
Liang Zhuang <bzhuangliang@gmail.com>
2026-05-15 16:50:28 -0700

    call __UUPSUpgradeable_init() in initialize

--- src/GopherStaking.sol
+++ src/GopherStaking.sol
@@ -54,7 +54,8 @@ contract GopherStaking is Initializable, AccessControlUpgradeable, UUPSUpgradeab
 
     function initialize(address admin, address _token, uint256 _rewardPerBlock) public initializer {
         __AccessControl_init();
-
+        __UUPSUpgradeable_init();
+        
         token = IERC20(_token);
         rewardPerBlock = _rewardPerBlock;
         lastRewardBlock = block.number;
